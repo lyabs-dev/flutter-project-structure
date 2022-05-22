e3464e9e5503c12e46e99b6f619ddbe87295d5c5
lyabs243 <loicyabili@outlook.com>
2022-05-22 12:34:06 +0200

    Update platform folders to the latest

---
+++ android/app/src/main/kotlin/com/example/test/test_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test.test_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/org/notifygroup/flutter_app_structure/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.notifygroup.flutter_app_structure
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
