dd794f53284b56e2a457f9296597aca2c1d069a6
Loïc Yabili <loicyabili@outlook.com>
2023-11-03 15:56:39 +0200

    Update platform folders

---
+++ android/app/src/main/kotlin/dev/lyabs/test_name/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.lyabs.test_name
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lyabs_dev/main.dart';
 import 'package:lyabs_dev/utils/my_material.dart';
 
-void main() async {
+void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     SettingsItem settings = await SettingsRepository().getSettings();
