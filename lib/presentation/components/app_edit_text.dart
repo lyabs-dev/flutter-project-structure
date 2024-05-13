a0a4a894f317052ccd10cd95d0ba739386eb55e4
Loic Yabili <loicyabili@gmail.com>
2024-05-13 16:50:22 +0200

    Enhance app edit text

--- lib/presentation/components/app_edit_text.dart
+++ lib/presentation/components/app_edit_text.dart
@@ -2,54 +2,59 @@ import 'package:flutter/services.dart';
 import 'package:structure/utils/my_material.dart';
 
 class AppEditText extends StatelessWidget {
+
   final String? hintText, labelText;
-  final TextEditingController controller;
+  final TextEditingController? controller;
   final bool isPassword, showCursor,autofocus;
   final TextInputType inputType;
   final Function(String value)? onChanged;
   final double? width,borderRadius, height;
-  final Color? color, backgroundColor, cursorColor;
+  final Color? backgroundColor, cursorColor;
   final InputBorder border, enabledBorder, focusedBorder;
-  final EdgeInsets? padding, contentPadding;
+  final EdgeInsets? padding;
   final Widget? suffixIcon;
   final TextStyle? hintStyle, labelStyle, style;
   final Widget? prefixIcon;
   final List<TextInputFormatter>? inputFormatters;
   final TextAlign? textAlign;
-  final int maxLines;
+  final int? maxLines, minLines, maxLength;
   final FocusNode? focusNode;
   final bool enabled;
 
-  const AppEditText(this.hintText, this.controller,
+  const AppEditText(
+      this.hintText,
       {
         super.key,
+        this.controller,
         this.isPassword = false,
-      this.inputType = TextInputType.text,
-      this.width,
+        this.inputType = TextInputType.text,
+        this.width,
         this.height,
-      this.backgroundColor,
+        this.backgroundColor,
         this.onChanged,
-      this.borderRadius = 0.0,
+        this.borderRadius = 0.0,
         this.autofocus = false,
         this.inputFormatters,
-      this.cursorColor,
-      this.padding,
-      this.contentPadding = const EdgeInsets.only(left: 10),
-      this.suffixIcon,
-      this.border = InputBorder.none,
-      this.enabledBorder = InputBorder.none,
-      this.focusedBorder = InputBorder.none,
-      this.hintStyle,
-      this.labelStyle,
-      this.style,
-      this.labelText,
-      this.prefixIcon,
-      this.textAlign = TextAlign.start,
-      this.color = Colors.grey,
-      this.maxLines = 1,
-      this.showCursor = true,
-      this.focusNode,
-      this.enabled = true});
+        this.cursorColor,
+        this.padding,
+        this.suffixIcon,
+        this.border = InputBorder.none,
+        this.enabledBorder = InputBorder.none,
+        this.focusedBorder = InputBorder.none,
+        this.hintStyle,
+        this.labelStyle,
+        this.style,
+        this.labelText,
+        this.prefixIcon,
+        this.textAlign = TextAlign.start,
+        this.maxLines = 1,
+        this.minLines = 1,
+        this.showCursor = true,
+        this.focusNode,
+        this.maxLength,
+        this.enabled = true
+      }
+    );
 
   @override
   Widget build(BuildContext context) {
@@ -71,20 +76,21 @@ class AppEditText extends StatelessWidget {
         inputFormatters: inputFormatters,
         maxLines: maxLines,
         autofocus: autofocus,
-        minLines: 1,
+        minLines: minLines,
         cursorColor: cursorColor,
         style: style,
         textAlign: textAlign!,
         enabled: enabled,
-
+        maxLength: maxLength,
         decoration: InputDecoration(
-          contentPadding: contentPadding,
           hintText: hintText,
           hintStyle: hintStyle,
           labelText: labelText,
           labelStyle: labelStyle,
+          hintMaxLines: 3,
           border: border,
           suffixIcon: suffixIcon,
+          prefixIcon: prefixIcon,
           enabledBorder: enabledBorder,
           focusedBorder: focusedBorder,
         ),
